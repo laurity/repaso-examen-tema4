3d9af15e8a77e2fe7558c0ff6867df6ed473aaeb
Laura López <lauralopez166@gmail.com>
2023-12-01 13:53:22 +0100

    guardar por si acaso

--- scripts/main.js
+++ scripts/main.js
@@ -20,14 +20,36 @@ vehiculo1.arrancar();
 vehiculo1.acelerar(140);
 vehiculo1.frenar();
 
-//Muestra de un objeto furgoneta 
+//Almacenar el objeto local storage con formato json
+//Variable de tipo Object de un objeto vehículo
+const vehiculoObject = {
+    marca: 'BMW',
+    modelo: 'z4',
+    color: 'Rojo',
+    fabricacion: 1992,
+    cilindrada: '2400'
+}
+
+//Almacenar el objeto en local storage - Guardar el JSON en el local storage
+localStorage.setItem('vehiculo', JSON.stringify(vehiculoObject));
+
+//Recuperar datos - Mostrar el Object en consola del JSON que hemos guardado en el local storage
+console.log(localStorage.getItem('vehiculo'));
+
+//Imprimimos por consola el JSON
+console.log(JSON.parse(localStorage.getItem('vehiculo')));
+
+
+//Muestra de un objeto furgoneta por consola 
 const furgoneta1 = new Furgoneta('Mercedes', 'Kangoo', 'Azul', 2014, '1800', 8);
 mostrarResultado('<br><br>-------------------Furgoneta 1--------------------------'); 
 furgoneta1.mostrarDatos();
 furgoneta1.arrancar();
 furgoneta1.acelerar(130);
 furgoneta1.frenar();
-console.log(furgoneta1.marca)
+
+//Guardar unicamente la clase furgoneta (por furgoneta)
+localStorage.setItem('marca-furgoneta1', JSON.stringify(furgoneta1.marca));
 
 //Muestra un objeto Todoterreno
 
